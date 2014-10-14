69d7821d8f0329318e329f88c2a6bfb5127f4e69
Tahmid Sadik <tahmidsadik112@gmail.com>
2014-10-14 16:34:21 +0600

    Fixed certificate errors.

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# A simple setup script to create an executable using PyQt4. This also
-# demonstrates the method for creating a Windows executable that does not have
-# an associated console.
-#
-# PyQt4app.py is a very simple type of PyQt4 application
-#
-# Run the build process by running the command 'python setup.py build'
-#
-# If everything works well you should find a subdirectory in the build
-# subdirectory that contains the files needed to run the application
-
 import sys
 from cx_Freeze import setup, Executable
 
@@ -20,8 +7,9 @@ if sys.platform == 'win32':
 
 options = {
     'build_exe': {
-        'includes': 'atexit'
-    }
+        'includes': 'atexit',
+        'include_files': 'cert/'
+    } 
 }
 
 executables = [
@@ -30,7 +18,7 @@ executables = [
 
 setup(name='SHTR.proto',
       version='0.0.1',
-      description='First prototype build of SHTR.proto. Expect bugs.',
+      description='SHTR.prototype 1 . Expect lots of bugs. :(',
       options=options,
       executables=executables
       )

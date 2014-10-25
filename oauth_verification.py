e5b5a285333eff2f7b2022c9c9d2305e7adccae4
Tahmid Sadik <tahmidsadik112@gmail.com>
2014-10-26 04:34:27 +0600

    PythonSimpleHTTPServer Blocks. Gonna use Cheerypy and PyDispatcher. Full Window Screenshot is working. Currently working on region based shot

--- oauth_verification.py
+++ oauth_verification.py
@@ -1,23 +1,44 @@
 __author__ = 'tahmid'
 
-import httplib2
 import urllib
-from googleapiclient import errors
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urlparse
+
+import httplib2
 from googleapiclient.discovery import build
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 
+
+CODE = ''
+
+class customHandler(BaseHTTPRequestHandler):
+    #handler for GET requests
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write("<center><h2>You have successfully logged in<h2><center>")
+        s =  urlparse.urlparse(self.requestline).query
+        code =  s.split('=', 1)[1].split(' ')[0]
+        CODE = code
+        return
+
+
 class Authenticator:
     def __init__(self):
+        self.verification_code = CODE
         self.path_to_client_secrect = 'client_secret.json'
         self.client_id = '1015702721813-davr8fgfgji5eih3ab52qnke423n53it.apps.googleusercontent.com'
         self.client_secrect = '1015702721813-davr8fgfgji5eih3ab52qnke423n53it.apps.googleusercontent.com'
         self.token_file_name = 'token.json'
-        self.redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+        self.redirect_uri = 'http://localhost:8080'
         self.storage = Storage(self.token_file_name)
         self.auth_scope = 'https://www.googleapis.com/auth/drive.file'
         self.flow = flow_from_clientsecrets(self.path_to_client_secrect, self.auth_scope, self.redirect_uri )
         self.creden = self.storage.get()
+        self.port = 7575
 
     def _isAlreadyLoggedIn(self):
         if(self.creden != None):
@@ -31,3 +52,12 @@ class Authenticator:
         http = httplib2.Http()
         http = self.creden.authorize(http)
         drive_service = build('drive', 'v2', http = http)
+
+    def collect_code(self):
+        self.verification_code =CODE
+
+    def start_auth_server(self):
+        server = HTTPServer(("", self.port), customHandler)
+        server._handle_request_noblock()
+        self.collect_code()
+
--- shoot.py
+++ shoot.py
@@ -1,19 +1,55 @@
 import sys
+from datetime import datetime
+
 from PySide.QtGui import *
 from PySide.QtWebKit import QWebView
-from oauth_verification import Authenticator
 from PySide.QtCore import QUrl
+from PySide.QtCore import QRect, QSize
+
+from oauth_verification import Authenticator
+
+
+class TransWidget(QWidget):
+    def __init__(self):
+        super(TransWidget, self).__init__()
+        self.setWindowOpacity(0.3)
+        self.setFocus()
+        self.showMaximized()
+        self.band = QRubberBand(QRubberBand.Rectangle, self)
+
+    def mousePressEvent(self, event):
+        super(TransWidget, self).mousePressEvent(event)
+        print "Mouse pressed"
+        self.origin = event.pos()
+        if not self.band:
+            self.band = QRubberBand(QRubberBand.Rectangle, self)
+        self.band.setGeometry(QRect(self.origin, QSize()))
+        self.band.show()
+
+    def mouseMoveEvent(self, event):
+        super(TransWidget, self).mouseMoveEvent(event)
+        self.band.setGeometry(QRect(self.origin, event.pos()).normalized())
+
+    def mouseReleaseEvent(self, event):
+        super(TransWidget, self).mouseReleaseEvent(event)
+        self.band.hide()
 
 class App:
     def __init__(self):
         self.app = QApplication(sys.argv)
         self.view = QWebView()
+        self.filename = ''
         icon = QIcon("shutter.png")
         menu = QMenu()
+        fullWindowShot = menu.addAction("Capture Full Window")
+        regionShot = menu.addAction("Capture a region")
         settingAction = menu.addAction("Settings")
         exitAction = menu.addAction("Exit")
+        fullWindowShot.triggered.connect(self.takeFullWindowShot)
+        regionShot.triggered.connect(self.takeRegionShot)
         settingAction.triggered.connect(self.setting)
         exitAction.triggered.connect(sys.exit)
+
 
         self.tray = QSystemTrayIcon()
         self.tray.setIcon(icon)
@@ -25,6 +61,14 @@ class App:
         self.view.load(QUrl(url))
         self.view.show()
         self.view.loadFinished.connect(self.load1)
+
+    def takeFullWindowShot(self):
+        self.filename = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.jpg')
+        p = QPixmap.grabWindow(QApplication.desktop().winId())
+        p.save(self.filename, 'jpg')
+
+    def takeRegionShot(self):
+        self.transWidget = TransWidget()
 
     def load1(self):
        self.view.urlChanged.connect(self.onUrlChange)
@@ -42,7 +86,16 @@ class App:
         self.dialog.show()
 
 if __name__ == "__main__":
-    app = App()
     auth = Authenticator()
-    app.redirect_to_permision_url(auth._get_permission_url())
-    app.start()
+    app = App()
+
+    if(auth._isAlreadyLoggedIn()):
+        app.start()
+    else:
+        m = QMessageBox()
+        m.setText("You are not logged into Google Drive")
+        m.setInformativeText("We detected that you are not logged into google drive. So we are redirecting you to log in first.")
+        m.setFocus()
+        m.exec_()
+        #app.redirect_to_permision_url(auth._get_permission_url())
+        app.start()
